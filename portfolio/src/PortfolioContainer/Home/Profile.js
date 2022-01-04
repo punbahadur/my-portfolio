12778319ca2986c8b522d70ebe7aff481d2f2083
poojan <poojanchhetri@gmail.com>
2022-01-04 02:21:36 -0700

    update

---
+++ portfolio/src/PortfolioContainer/Home/Profile.js
@@ -0,0 +1,66 @@
+import React from "react";
+import Typical from "react-typical";
+
+export default function Profile() {
+  return (
+    <div className="profile-container">
+      <div className="profile-parent">
+        <div className="profile-details">
+          <div className="colz">
+            <a href="https://www.facebook.com/poojan.chhetri.779/">
+              <i className="fa fa-facebook-square"> </i>{" "}
+            </a>{" "}
+            <a href="https://www.google.com/">
+              <i className="fa fa-google-plus-square"> </i>{" "}
+            </a>{" "}
+            <a href="https://www.instagram.com/poo__jan/">
+              <i className="fa fa-instagram"> </i>{" "}
+            </a>{" "}
+            <a href="https://www.youtube.com/watch?v=7uJGjbkp0-U">
+              <i className="fa fa-youtube-square"> </i>{" "}
+            </a>{" "}
+            <a href="#">
+              <i className="fa fa-twitter"> </i>{" "}
+            </a>{" "}
+          </div>{" "}
+          <div className="profile-details-name">
+            <span className="primary-text">
+              {" "}
+              Hello, I am <span className="highlighted-text"> Pun </span>{" "}
+            </span>{" "}
+          </div>{" "}
+          <div className="profile-details-role">
+            <span className="primary-text">
+              {" "}
+              <h1>
+                {" "}
+                <Typical
+                  loop={Infinity}
+                  steps={[
+                    "Enthusiastic Dev 😎",
+                    1000,
+                    "Full Stack Developer 💻",
+                    1000,
+                    "MERN Stack Dev 💯",
+                    1000,
+                    "Cross Platform Dev 🧠",
+                    1000,
+                    "React/React Native Dev 📱",
+                    1000,
+                  ]}
+                />{" "}
+              </h1>{" "}
+              <span className="profile-role-tagline">
+                Knack of building applications with front and back end
+                operations.{" "}
+              </span>{" "}
+            </span>{" "}
+          </div>{" "}
+          <div className="profile-options">
+            <button className="btn primary-btn"> Hire Me </button>{" "}
+          </div>{" "}
+        </div>{" "}
+      </div>{" "}
+    </div>
+  );
+}
